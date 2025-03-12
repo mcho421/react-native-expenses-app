b53153dcac19260da6a6a2c267c272b2039f968d
Mathew Chong <mathewchong.dev@gmail.com>
2025-03-13 08:16:50 +1100

    Remove modal styling

--- screens/ManageExpenseScreen.js
+++ screens/ManageExpenseScreen.js
@@ -1,5 +1,5 @@
 import { useLayoutEffect } from "react";
-import { Button, Text, View } from "react-native";
+import { Button, View } from "react-native";
 import IconButton from "../components/UI/IconButton";
 import { GlobalStyles } from "../constants/styles";
 
@@ -20,65 +20,47 @@ export default function ManageExpense({ route, navigation }) {
   return (
     <View
       style={{
-        backgroundColor: "darkblue",
-        borderTopLeftRadius: 16,
-        borderTopRightRadius: 16,
-        overflow: "hidden",
-        marginTop: 12,
+        backgroundColor: GlobalStyles.colors.primary800,
         flex: 1,
-        elevation: 6,
+        padding: 24,
       }}
     >
-      <View style={{ backgroundColor: "blue", padding: 8 }}>
-        <Text
-          style={{
-            color: "white",
-            textAlign: "center",
-            fontSize: 16,
-            fontWeight: "bold",
-          }}
-        >
-          {isEditing ? "Edit Expense" : "Add Expense"}
-        </Text>
+      <View
+        style={{
+          flexDirection: "row",
+          justifyContent: "center",
+          gap: 18,
+        }}
+      >
+        <View style={{ width: "35%" }}>
+          <Button
+            title="Cancel"
+            color="darkblue"
+            onPress={() => navigation.goBack()}
+          />
+        </View>
+        <View style={{ width: "35%" }}>
+          <Button title={isEditing ? "Update" : "Add"} />
+        </View>
       </View>
-      <View style={{ padding: 16 }}>
+      {isEditing && (
         <View
           style={{
-            flexDirection: "row",
-            justifyContent: "center",
-            gap: 18,
+            marginTop: 16,
+            paddingTop: 8,
+            borderTopWidth: 2,
+            borderTopColor: GlobalStyles.colors.primary200,
+            alignItems: "center",
           }}
         >
-          <View style={{ width: "35%" }}>
-            <Button
-              title="Cancel"
-              color="darkblue"
-              onPress={() => navigation.goBack()}
-            />
-          </View>
-          <View style={{ width: "35%" }}>
-            <Button title={isEditing ? "Update" : "Add"} />
-          </View>
+          <IconButton
+            icon="trash"
+            color={GlobalStyles.colors.error500}
+            size={36}
+            onPress={deleteExpenseHandler}
+          />
         </View>
-        {isEditing && (
-          <View
-            style={{
-              marginTop: 16,
-              paddingTop: 8,
-              borderTopWidth: 2,
-              borderTopColor: GlobalStyles.colors.primary200,
-              alignItems: "center",
-            }}
-          >
-            <IconButton
-              icon="trash"
-              color={GlobalStyles.colors.error500}
-              size={36}
-              onPress={deleteExpenseHandler}
-            />
-          </View>
-        )}
-      </View>
+      )}
     </View>
   );
 }
