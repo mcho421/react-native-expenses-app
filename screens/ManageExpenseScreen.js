5fd224117ebae79321013c77d7e8ea8f95e1fe4c
Mathew Chong <mathewchong.dev@gmail.com>
2025-03-13 08:04:29 +1100

    Combine editing and adding html templates

--- screens/ManageExpenseScreen.js
+++ screens/ManageExpenseScreen.js
@@ -12,119 +12,74 @@ export default function ManageExpense({ route, navigation }) {
     });
   }, [navigation, isEditing]);
 
-  if (!isEditing) {
-    return (
-      <View
-        style={{
-          backgroundColor: "darkblue",
-          borderTopLeftRadius: 16,
-          borderTopRightRadius: 16,
-          overflow: "hidden",
-          marginTop: 12,
-          flex: 1,
-          elevation: 6,
-        }}
-      >
-        <View style={{ backgroundColor: "blue", padding: 8 }}>
-          <Text
-            style={{
-              color: "white",
-              textAlign: "center",
-              fontSize: 16,
-              fontWeight: "bold",
-            }}
-          >
-            Add Expense
-          </Text>
-        </View>
-        <View style={{ padding: 16 }}>
-          <View
-            style={{
-              flexDirection: "row",
-              justifyContent: "center",
-              gap: 18,
-            }}
-          >
-            <View style={{ width: "35%" }}>
-              <Button
-                title="Cancel"
-                color="darkblue"
-                onPress={() => navigation.goBack()}
-              />
-            </View>
-            <View style={{ width: "35%" }}>
-              <Button title="Add" />
-            </View>
-          </View>
-        </View>
+  return (
+    <View
+      style={{
+        backgroundColor: "darkblue",
+        borderTopLeftRadius: 16,
+        borderTopRightRadius: 16,
+        overflow: "hidden",
+        marginTop: 12,
+        flex: 1,
+        elevation: 6,
+      }}
+    >
+      <View style={{ backgroundColor: "blue", padding: 8 }}>
+        <Text
+          style={{
+            color: "white",
+            textAlign: "center",
+            fontSize: 16,
+            fontWeight: "bold",
+          }}
+        >
+          {isEditing ? "Edit Expense" : "Add Expense"}
+        </Text>
       </View>
-    );
-  } else {
-    return (
-      <View
-        style={{
-          backgroundColor: "darkblue",
-          borderTopLeftRadius: 16,
-          borderTopRightRadius: 16,
-          overflow: "hidden",
-          marginTop: 12,
-          flex: 1,
-          elevation: 6,
-        }}
-      >
-        <View style={{ backgroundColor: "blue", padding: 8 }}>
-          <Text
-            style={{
-              color: "white",
-              textAlign: "center",
-              fontSize: 16,
-              fontWeight: "bold",
-            }}
-          >
-            Edit Expense
-          </Text>
-        </View>
-        <View style={{ padding: 16 }}>
-          <View
-            style={{
-              flexDirection: "row",
-              justifyContent: "center",
-              gap: 18,
-            }}
-          >
-            <View style={{ width: "35%" }}>
-              <Button
-                title="Cancel"
-                color="darkblue"
-                onPress={() => navigation.goBack()}
-              />
-            </View>
-            <View style={{ width: "35%" }}>
-              <Button title="Update" />
-            </View>
-          </View>
-          <View
-            style={{
-              borderBottomColor: "white",
-              borderBottomWidth: 1,
-              margin: 12,
-            }}
-          />
-          <View
-            style={{
-              flexDirection: "row",
-              justifyContent: "center",
-            }}
-          >
-            <Pressable
-              style={{ padding: 6 }}
+      <View style={{ padding: 16 }}>
+        <View
+          style={{
+            flexDirection: "row",
+            justifyContent: "center",
+            gap: 18,
+          }}
+        >
+          <View style={{ width: "35%" }}>
+            <Button
+              title="Cancel"
+              color="darkblue"
               onPress={() => navigation.goBack()}
-            >
-              <Ionicons name="trash" color="red" size={28}></Ionicons>
-            </Pressable>
+            />
+          </View>
+          <View style={{ width: "35%" }}>
+            <Button title={isEditing ? "Update" : "Add"} />
           </View>
         </View>
+        {isEditing && (
+          <>
+            <View
+              style={{
+                borderBottomColor: "white",
+                borderBottomWidth: 1,
+                margin: 12,
+              }}
+            />
+            <View
+              style={{
+                flexDirection: "row",
+                justifyContent: "center",
+              }}
+            >
+              <Pressable
+                style={{ padding: 6 }}
+                onPress={() => navigation.goBack()}
+              >
+                <Ionicons name="trash" color="red" size={28}></Ionicons>
+              </Pressable>
+            </View>
+          </>
+        )}
       </View>
-    );
-  }
+    </View>
+  );
 }
