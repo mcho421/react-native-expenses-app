1d9ffba5081a8451ec261dbea3eddd04cc99d401
Mathew Chong <mathewchong.dev@gmail.com>
2025-03-13 06:13:00 +1100

    Adjust ExpenseItem colours to purple theme

--- components/ExpensesOutput/ExpenseItem.js
+++ components/ExpensesOutput/ExpenseItem.js
@@ -1,35 +1,55 @@
 import { View, Text, Pressable } from "react-native";
+import { GlobalStyles } from "../../constants/styles";
 
 export default function ExpenseItem({ description, date, amount, onPress }) {
   return (
     <Pressable onPress={onPress}>
       <View
         style={{
-          backgroundColor: "blue",
+          backgroundColor: GlobalStyles.colors.primary500,
           flexDirection: "row",
           padding: 12,
-          borderRadius: 4,
+          borderRadius: 6,
           width: "100%",
           justifyContent: "space-between",
-          marginVertical: 6,
+          marginVertical: 8,
+          elevation: 3,
+          shadowColor: GlobalStyles.colors.gray500,
+          shadowRadius: 4,
+          shadowOffset: { width: 1, height: 1 },
+          shadowOpacity: 0.4,
         }}
       >
         <View>
-          <Text style={{ color: "white", fontWeight: "bold" }}>
+          <Text
+            style={{
+              color: GlobalStyles.colors.primary50,
+              fontWeight: "bold",
+              marginBottom: 4,
+            }}
+          >
             {description}
           </Text>
-          <Text style={{ color: "white" }}>{date}</Text>
+          <Text style={{ color: GlobalStyles.colors.primary50 }}>{date}</Text>
         </View>
         <View
           style={{
+            paddingHorizontal: 12,
+            paddingVertical: 4,
             backgroundColor: "white",
-            width: "20%",
             borderRadius: 4,
             justifyContent: "center",
             alignItems: "center",
           }}
         >
-          <Text style={{ color: "purple", fontWeight: "bold" }}>{amount}</Text>
+          <Text
+            style={{
+              color: GlobalStyles.colors.primary500,
+              fontWeight: "bold",
+            }}
+          >
+            {amount}
+          </Text>
         </View>
       </View>
     </Pressable>
