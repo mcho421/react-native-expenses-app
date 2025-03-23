db1ce325966829331f2f84267412146d4f7092ec
Mathew Chong <mathewchong.dev@gmail.com>
2025-03-23 17:34:35 +1100

    Add form inputs to Manage Expenses

--- screens/ManageExpenseScreen.js
+++ screens/ManageExpenseScreen.js
@@ -4,6 +4,7 @@ import IconButton from "../components/UI/IconButton";
 import { GlobalStyles } from "../constants/styles";
 import Button from "../components/UI/Button";
 import { ExpensesContext } from "../store/expenses-context";
+import ExpenseForm from "../components/ManageExpense/ExpenseForm";
 
 export default function ManageExpense({ route, navigation }) {
   const editedExpenseId = route.params?.expenseId;
@@ -50,6 +51,7 @@ export default function ManageExpense({ route, navigation }) {
         padding: 24,
       }}
     >
+      <ExpenseForm />
       <View
         style={{
           flexDirection: "row",
