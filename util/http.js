b9d2c90a1bb2e743b709b94614d47226014def92
Mathew Chong <mathewchong.dev@gmail.com>
2025-03-27 08:30:14 +1100

    Use id from Firebase when adding expense

--- util/http.js
+++ util/http.js
@@ -3,8 +3,13 @@ import axios from "axios";
 const BACKEND_URL =
   "https://react-native-course-32dda-default-rtdb.asia-southeast1.firebasedatabase.app";
 
-export function storeExpense(expenseData) {
-  axios.post(BACKEND_URL + "/expenses.json", expenseData);
+export async function storeExpense(expenseData) {
+  const response = await axios.post(
+    BACKEND_URL + "/expenses.json",
+    expenseData
+  );
+  const id = response.data.name;
+  return id;
 }
 
 export async function fetchExpenses() {
